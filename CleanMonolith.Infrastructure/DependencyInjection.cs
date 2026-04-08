6a6d3453e6de12c94fccc935cf3a9a8a12ef56bd
Tamil selvan G <tamilselvang@novatechset.com>
2026-04-08 10:12:15 +0530

    add dependency

--- CleanMonolith.Infrastructure/DependencyInjection.cs
+++ CleanMonolith.Infrastructure/DependencyInjection.cs
@@ -1,24 +1,46 @@
 using CleanMonolith.Application.Interfaces;
+using CleanMonolith.Application.Services;
+using CleanMonolith.Infrastructure.Entity;
 using CleanMonolith.Infrastructure.Identity;
 using CleanMonolith.Infrastructure.Persistence;
 using CleanMonolith.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
 
 namespace CleanMonolith.Infrastructure;
 
 public static class DependencyInjection
 {
+  
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>(options =>
+        services.AddDbContext<nEdit_DEVContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
-        services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
-        
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
         services.AddScoped<IJwtProvider, JwtProvider>();
+        services.AddScoped<IChatService, ChatService>();
+        services.AddScoped<IAIService, AIService>();
+
+        services.AddHttpClient<IAIService, AIService>(client =>
+        {
+            client.BaseAddress = new Uri("https://api.openai.com/");
+            client.Timeout = TimeSpan.FromSeconds(60);
+        })
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    return new HttpClientHandler
+    {
+        // 🔥 ONLY FOR LOCAL DEV
+        ServerCertificateCustomValidationCallback =
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+});
+
 
         return services;
     }
